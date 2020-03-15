c852e34d93226baa92cb5196813226b87fceb8a7
lt <1029563229@qq.com>
2020-03-16 07:49:06 +0800

    react 源码解析

---
+++ Source-Code/react/collects/react/ReactBaseClasses.js
@@ -0,0 +1,17 @@
+function Component(props, context, updater) {
+  this.props = props;
+  this.context = context;
+  this.refs = emptyObject;
+  this.updater = updater || ReactNoopUpdateQueue;
+}
+
+Component.prototype.isReactComponent = {};
+
+Component.prototype.setState = function(partialState, callback) {
+  this.updater.enqueueSetState(this, partialState, callback, 'setState');
+};
+
+Component.prototype.forceUpdate = function(callback) {
+  this.updater.enqueueForceUpdate(this, callback, 'forceUpdate');
+};
+
