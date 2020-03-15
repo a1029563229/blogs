659b991d8476d4c14443911b7c9aea436afe6901
lt <1029563229@qq.com>
2020-03-15 11:57:11 +0800

    react 源码解析

---
+++ Source-Code/react/demo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    console.log(this);
+    return (
+      <section>Hello World</section>
+    )
+  }
+}
+
+export default App;
+
