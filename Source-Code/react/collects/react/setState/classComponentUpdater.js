aedc883e9bbfd471eb8711c0db8cf039df1449b9
lt <1029563229@qq.com>
2020-03-26 10:19:41 +0800

    源码阅读

---
+++ Source-Code/react/collects/react/setState/classComponentUpdater.js
@@ -0,0 +1,69 @@
+const classComponentUpdater = {
+  isMounted,
+  enqueueSetState(inst, payload, callback) {
+    const fiber = getInstance(inst);
+    const currentTime = requestCurrentTimeForUpdate();
+    const suspenseConfig = requestCurrentSuspenseConfig();
+    const expirationTime = computeExpirationForFiber(
+      currentTime,
+      fiber,
+      suspenseConfig,
+    );
+
+    const update = createUpdate(expirationTime, suspenseConfig);
+    update.payload = payload;
+    if (callback !== undefined && callback !== null) {
+      update.callback = callback;
+    }
+
+    enqueueUpdate(fiber, update);
+    scheduleUpdateOnFiber(fiber, expirationTime);
+  },
+  enqueueReplaceState(inst, payload, callback) {
+    const fiber = getInstance(inst);
+    const currentTime = requestCurrentTimeForUpdate();
+    const suspenseConfig = requestCurrentSuspenseConfig();
+    const expirationTime = computeExpirationForFiber(
+      currentTime,
+      fiber,
+      suspenseConfig,
+    );
+
+    const update = createUpdate(expirationTime, suspenseConfig);
+    update.tag = ReplaceState;
+    update.payload = payload;
+
+    if (callback !== undefined && callback !== null) {
+      if (__DEV__) {
+        warnOnInvalidCallback(callback, 'replaceState');
+      }
+      update.callback = callback;
+    }
+
+    enqueueUpdate(fiber, update);
+    scheduleUpdateOnFiber(fiber, expirationTime);
+  },
+  enqueueForceUpdate(inst, callback) {
+    const fiber = getInstance(inst);
+    const currentTime = requestCurrentTimeForUpdate();
+    const suspenseConfig = requestCurrentSuspenseConfig();
+    const expirationTime = computeExpirationForFiber(
+      currentTime,
+      fiber,
+      suspenseConfig,
+    );
+
+    const update = createUpdate(expirationTime, suspenseConfig);
+    update.tag = ForceUpdate;
+
+    if (callback !== undefined && callback !== null) {
+      if (__DEV__) {
+        warnOnInvalidCallback(callback, 'forceUpdate');
+      }
+      update.callback = callback;
+    }
+
+    enqueueUpdate(fiber, update);
+    scheduleUpdateOnFiber(fiber, expirationTime);
+  },
+};
--- Source-Code/react/demo/src/App.js
+++ Source-Code/react/demo/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      content: "Hello World"
+    };
+  }
+
+  changeContent() {
+    debugger;
+    this.setState({
+      content: "Update Content"
+    });
+  }
+
   render() {
-    console.log(this);
+    const { content } = this.state;
     return (
-      <section>Hello World</section>
-    )
+      <section>
+        <div>{content}</div>
+        <button onClick={() => this.changeContent()}>调用 setState</button>
+      </section>
+    );
   }
 }
 
 export default App;
-
