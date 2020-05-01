ce421018a231d085b24e4fc165518eb5454d195f
lt <1029563229@qq.com>
2020-05-01 15:36:34 +0800

    caddy 使用教程

---
+++ BestPractices/caddy/demo/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  mounted() {
+    fetch("http://dev-api-mall.jt-gmall.com/vegetable/list?page=1&pageSize=20")
+      .then(res => res.json())
+      .then(res => {
+        console.log(res);
+      });
+  }
+};
+</script>
+
+<style>
+</style>
