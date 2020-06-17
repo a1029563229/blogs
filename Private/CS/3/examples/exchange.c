38728423d0f6cac6b9dcf68e6a2fcda7b171131b
lt <1029563229@qq.com>
2020-06-17 09:41:08 +0800

    📖 压入和弹出栈

---
+++ Private/CS/3/examples/exchange.c
@@ -0,0 +1,7 @@
+int main() {
+  int a = 1;
+  int *p = &a;
+
+  int b = 1;
+  return 0;
+}
---
+++ Private/CS/3/examples/exchange.s
@@ -0,0 +1,19 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 10, 15	sdk_version 10, 15, 4
+	.globl	_main                   ## -- Begin function main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## %bb.0:
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register %rbp
+	xorl	%eax, %eax
+	popq	%rbp
+	retq
+	.cfi_endproc
+                                        ## -- End function
+
+.subsections_via_symbols
