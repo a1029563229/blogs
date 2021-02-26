57f87ffa8ed8b8f1cfeda42b1d929ea69ff030db
lt <1029563229@qq.com>
2021-02-26 10:37:45 +0800

    📖 读书笔记

---
+++ Private/C/1/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* when fahr = 0, 20, ..., 300, 
+print fahr and celsius compare table */
+int main() {
+  int fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0; /* lower limit of thermometer */
+  upper = 300; /* upper limit of thermometer */
+  step = 20; /* step */
+
+  fahr = lower;
+  while(fahr <= upper) {
+
+  }
+}
